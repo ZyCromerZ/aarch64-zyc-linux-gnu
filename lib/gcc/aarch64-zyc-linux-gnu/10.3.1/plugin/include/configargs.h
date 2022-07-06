f03f435beaee19841cefb9fc9708c33edb042e1b
ZyCromerZ <neetroid97@gmail.com>
2022-07-06 13:17:58 +0700

    Update to https://github.com/gcc-mirror/gcc/commit/0c4298386cfdc759ac13d9614b7add715acf8155
        GCC VERSION: aarch64-zyc-linux-gnu-gcc (ZyC-GCC) 10.4.1 20220706
        GCC COMMIT URL: https://github.com/gcc-mirror/gcc/commit/0c4298386cfdc759ac13d9614b7add715acf8155
    
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- lib/gcc/aarch64-zyc-linux-gnu/10.3.1/plugin/include/configargs.h
+++
@@ -1,7 +0,0 @@
-/* Generated automatically. */
-static const char configuration_arguments[] = "../gcc/configure --disable-multilib --disable-werror --with-pkgversion=ZyC-GCC CFLAGS='-g0 -O3 -fstack-protector-strong -fuse-linker-plugin -ffunction-sections -fdata-sections' CXXFLAGS='-g0 -O3 -fstack-protector-strong -fuse-linker-plugin -ffunction-sections -fdata-sections' --enable-languages=c,c++ --target=aarch64-zyc-linux-gnu --prefix=/__w/gcc-builder/gcc-builder/aarch64-zyc-linux-gnu";
-static const char thread_model[] = "posix";
-
-static const struct {
-  const char *name, *value;
-} configure_default_options[] = { { NULL, NULL} };
