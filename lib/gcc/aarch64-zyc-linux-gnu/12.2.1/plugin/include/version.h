be0693975d466cb98f842ad40acc9958a67f13b5
ZyCromerZ <neetroid97@gmail.com>
2023-02-09 13:17:08 +0700

    Update to https://github.com/gcc-mirror/gcc/commit/336162e701a81320ced8336d72bbce562b9be15d
        GCC VERSION: aarch64-zyc-linux-gnu-gcc (ZyC-GCC) 12.2.1 20230209
        GCC COMMIT URL: https://github.com/gcc-mirror/gcc/commit/336162e701a81320ced8336d72bbce562b9be15d
    
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- lib/gcc/aarch64-zyc-linux-gnu/12.2.1/plugin/include/version.h
+++ lib/gcc/aarch64-zyc-linux-gnu/12.2.1/plugin/include/version.h
@@ -9,7 +9,7 @@
 BASEVER, DATESTAMP, DEVPHASE, and REVISION are defined by the
 Makefile.  */
 
-#define version_string "12.2.1 20230208"
+#define version_string "12.2.1 20230209"
 #define pkgversion_string "(ZyC-GCC) "
 
 /* This is the location of the online document giving instructions for
